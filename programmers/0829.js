6f1b0046474ecd23295b0c2d562663066571d8df
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2022-08-29 12:28:44 +0900

    updated "object to map"

--- programmers/0829.js
+++ programmers/0829.js
@@ -6,37 +6,63 @@ let choices1 = [5, 3, 2, 7, 5];
 // 3번 지표	제이지형(J), 무지형(M)
 // 4번 지표	어피치형(A), 네오형(N)
 solution(survey1, choices1);
+// function solution(survey, choices) {
+//   let obj = {};
+//   choices.forEach((e, i) => {
+//     e > 3
+//       ? (obj[survey[i].split("")[1]] =
+//           (obj[survey[i].split("")[1]] || 0) + e - 4)
+//       : (obj[survey[i].split("")[0]] =
+//           (obj[survey[i].split("")[0]] || 0) + (-e + 4));
+//   });
+//   var answer = "";
+//   if (obj["R"] >= obj["T"] || obj["T"] == undefined) {
+//     answer += "R";
+//   } else {
+//     answer += "T";
+//   }
+//   if (obj["C"] >= obj["F"] || obj["F"] == undefined) {
+//     answer += "C";
+//   } else {
+//     answer += "F";
+//   }
+//   if (obj["J"] >= obj["M"] || obj["M"] == undefined) {
+//     answer += "J";
+//   } else {
+//     answer += "M";
+//   }
+//   if (obj["A"] >= obj["N"] || obj["N"] == undefined) {
+//     answer += "A";
+//   } else {
+//     answer += "N";
+//   }
+//   return answer;
+// }
+// map 함수 활용해서 만들어보자
 function solution(survey, choices) {
-  let obj = {};
-  choices.forEach((e, i) => {
-    e > 3
-      ? (obj[survey[i].split("")[1]] =
-          (obj[survey[i].split("")[1]] || 0) + e - 4)
-      : (obj[survey[i].split("")[0]] =
-          (obj[survey[i].split("")[0]] || 0) + (-e + 4));
+  // let obj = {};
+  let obj = new Map();
+  // choices.forEach((e, i) => {
+  //   e > 3
+  //     ? (obj[survey[i].split("")[1]] =
+  //         (obj[survey[i].split("")[1]] || 0) + e - 4)
+  //     : (obj[survey[i].split("")[0]] =
+  //         (obj[survey[i].split("")[0]] || 0) + (-e + 4));
+  // });
+
+  //유형을 알려 줬기 때문에 map 함수에 set 을 해주고 시작 기존은 undefined 가 있어서 비교문이 복잡해졌음
+  ["R", "T", "C", "F", "J", "M", "A", "N"].forEach((item) => obj.set(item, 0));
+  choices.forEach((item, index) => {
+    let [A, B] = survey[index].split("");
+    item > 4
+      ? obj.set(B, obj.get(B) + item - 4)
+      : obj.set(A, obj.get(A) + 4 - item);
   });
   var answer = "";
-  console.log(obj);
-  if (obj["R"] >= obj["T"] || obj["T"] == undefined) {
-    answer += "R";
-  } else {
-    answer += "T";
-  }
-  if (obj["C"] >= obj["F"] || obj["F"] == undefined) {
-    answer += "C";
-  } else {
-    answer += "F";
-  }
-  if (obj["J"] >= obj["M"] || obj["M"] == undefined) {
-    answer += "J";
-  } else {
-    answer += "M";
-  }
-  if (obj["A"] >= obj["N"] || obj["N"] == undefined) {
-    answer += "A";
-  } else {
-    answer += "N";
-  }
+  answer += obj.get("R") >= obj.get("T") ? "R" : "T";
+  answer += obj.get("C") >= obj.get("F") ? "C" : "F";
+  answer += obj.get("J") >= obj.get("M") ? "J" : "M";
+  answer += obj.get("A") >= obj.get("N") ? "A" : "N";
   console.log(answer);
   return answer;
 }
